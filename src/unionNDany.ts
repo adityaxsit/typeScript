016676e542aab949e5ab63e497f1a8783b9d6cbe
adityaxsit <1si24cs006@sit.ac.in>
2026-07-08 03:01:57 +0530

    union any

---
+++ src/unionNDany.ts
@@ -0,0 +1,17 @@
+//union
+let cost:string|number=1000 //'1k' can be string or number
+let apiRequest:'pending'|'success'|'failure'="pending"  //it can be only one of the three;;
+ // error :  apiRequest="done"  ;cz done is not there 
+
+
+
+ //any
+const orders=['23','32','45','89']
+let currentOrder1; //any 
+let currentOrder:string | undefined;
+
+for(let order of orders){
+    if(order==='23'){
+        currentOrder=order;
+    }
+}
