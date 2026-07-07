cae7dbfbff77d9007e33f783c75389663d961dbf
adityaxsit <1si24cs006@sit.ac.in>
2026-07-08 01:46:32 +0530

    basic syntax

--- 1_Basic/1_Basic.ts
+++ 1_Basic/1_Basic.ts
@@ -1,3 +1,5 @@
-var myuser = { name: "hitesh", age: 10 };
-console.log("Hitesh");
-//console.log(user.email);
+function greet(name:string):string{
+    return `Hello ${name}`
+}
+
+console.log(greet("Aditya"));
