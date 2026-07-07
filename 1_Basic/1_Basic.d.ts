ad463f1e24a802bc42f933eab5a8f64c6a9da955
adityaxsit <1si24cs006@sit.ac.in>
2026-07-08 02:46:39 +0530

    type inference ND type annotation

---
+++ 1_Basic/1_Basic.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=1_Basic.d.ts.map
---
+++ 1_Basic/1_Basic.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(name) {
+    return `Hello ${name}`;
+}
+console.log(greet("Aditya"));
+//# sourceMappingURL=1_Basic.js.map
